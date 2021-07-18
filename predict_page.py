6aa1d39ae7820289fd94b88b87ccf538904ca8b9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-18 14:03:00 +0530

    files

---
+++ predict_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    
+    return data
+
+data = load_model()
