e1b9fd0814e53dc5d949d0065addd13cc79fa73a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-18 14:14:37 +0530

    slider & select box

--- predict_page.py
+++ predict_page.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pickle
 import numpy as np
 
+
 def load_model():
     with open('saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
-    
+
     return data
+
 
 data = load_model()
 
@@ -14,7 +16,37 @@ regressor = data["model"]
 le_country = data["le_country"]
 le_education = data["le_education"]
 
+
 def show_predict_page():
     st.title("Software Developer Salary Prediction")
 
     st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "United States",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+    )
+
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+
+    experience = st.slider("Years Of Experience", 0, 50, 3)
