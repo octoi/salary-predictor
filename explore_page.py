6ea5799b9ac68ee729f69e61bd9d85a961568bf5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-18 14:21:49 +0530

    cleaners

---
+++ explore_page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def shorten_categories(categories, cutoff):
+    categorical_map = {}
+    for i in range(len(categories)):
+        if categories.values[i] >= cutoff:
+            categorical_map[categories.index[i]] = categories.index[i]
+        else:
+            categorical_map[categories.index[i]] = 'Other'
+    return categorical_map
+
+
+def clean_experience(x):
+    if x == 'More than 50 years':
+        return 50
+    if x == 'Less than 1 year':
+        return 0.5
+    return float(x)
+
+
+def clean_education(x):
+    if 'Bachelor’s degree' in x:
+        return 'Bachelor’s degree'
+    if 'Master’s degree' in x:
+        return 'Master’s degree'
+    if 'Professional degree' in x or 'Other doctoral' in x:
+        return 'Post grad'
+    return 'Less than a Bachelors'
